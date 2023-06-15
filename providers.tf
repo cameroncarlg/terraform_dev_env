b0526a2486f208b6ce4fbb48ea015f09ce096404
Cameron Guilbeau <cameroncarlg@gmail.com>
2023-06-14 20:20:39 -0500

    fixed credentials issue

--- providers.tf
+++ providers.tf
@@ -12,5 +12,6 @@ provider "aws" {
   region                   = "us-east-1"
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "default"
 }
 
